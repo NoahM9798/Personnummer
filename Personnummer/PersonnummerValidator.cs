3467030285afd7f476a7778d79f0c05c5f145f20
Delcio Vieira <din.epost@example.[200~$ git config --global user.name Ditt>
2026-01-15 10:31:28 +0100

    La tillbaks DateValid

--- Personnummer/PersonnummerValidator.cs
+++ Personnummer/PersonnummerValidator.cs
@@ -12,5 +12,27 @@ namespace Personnummer
         {
             return personnummer != null && personnummer.Length == 10;
         }
+
+        public bool IsDateValid(string pnr)
+        {
+            // PNR format: YYMMDD-XXXX
+            // Index:      0123456789...
+
+            // Hämta MÅNAD (Tecken på plats 2 och 3)
+            string monthString = pnr.Substring(2, 2);
+            int month = int.Parse(monthString);
+
+            // Hämta DAG (Tecken på plats 4 och 5)
+            string dayString = pnr.Substring(4, 2);
+            int day = int.Parse(dayString);
+
+            // Kolla Månad (1 - 12)
+            bool monthOk = (month >= 1 && month <= 12);
+
+            // Kolla Dag (1 - 31)
+            bool dayOk = (day >= 1 && day <= 31);
+
+            return monthOk && dayOk;
+        }
     }
 }
