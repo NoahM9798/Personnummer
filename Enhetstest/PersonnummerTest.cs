013b89969feeb47c817cfe2281bcfb35de83322e
Delcio Vieira <din.epost@example.[200~$ git config --global user.name Ditt>
2026-01-15 11:55:11 +0100

    Lade till enhetstester för IsDateValid

--- Enhetstest/PersonnummerTest.cs
+++ Enhetstest/PersonnummerTest.cs
@@ -17,5 +17,44 @@ namespace Enhetstest
             Assert.True(result);
 
         }
+
+        [Fact]
+        public void IsDateValid_ReturnTrue_CorrectDate()
+        {
+            // Arrange
+            var validator = new PersonnummerValidator();
+
+            // Act (Testar 18:e december, vilket är giltigt)
+            bool result = validator.IsDateValid("8112189876");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsDateValid_ReturnFalse_InvalidMonth()
+        {
+            // Arrange
+            var validator = new PersonnummerValidator();
+
+            // Act (Testar månad 13, vilket inte finns)
+            bool result = validator.IsDateValid("8113019876");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsDateValid_ReturnFalse_InvalidDay()
+        {
+            // Arrange
+            var validator = new PersonnummerValidator();
+
+            // Act (Testar dag 32, vilket inte finns)
+            bool result = validator.IsDateValid("8101329876");
+
+            // Assert
+            Assert.False(result);
+        }
     }
 }
