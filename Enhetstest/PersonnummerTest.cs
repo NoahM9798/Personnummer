ab7a81736899df813b0f3753eb2e6b91dfee4688
Tuva-Li Eriksson <eriksson.tuvali@gmail.com>
2026-01-15 12:07:47 +0100

    Lagt till och verifierat enhetstester för IsControlDigitValid

--- Enhetstest/PersonnummerTest.cs
+++ Enhetstest/PersonnummerTest.cs
@@ -1,4 +1,5 @@
 ﻿using Personnummer;
+using Xunit;
 
 namespace Enhetstest
 {
@@ -16,6 +17,34 @@ namespace Enhetstest
             //Assert
             Assert.True(result);
 
+        }
+        //---Tester för IsControldigitValid---
+        [Fact]
+        public void IsControlDigitValid_ReturnTrue_WhenDigitIsCorrect()
+        {
+            // Arrange
+            var validator = new PersonnummerValidator();
+            long validPnr = 8112189876; // Ett giltigt testnummer
+
+            // Act
+            bool result = validator.IsControlDigitValid(validPnr);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsControlDigitValid_ReturnFalse_WhenDigitIsWrong()
+        {
+            // Arrange
+            var validator = new PersonnummerValidator();
+            long invalidPnr = 8112189877; // Felaktig sista siffra (ska vara 6)
+
+            // Act
+            bool result = validator.IsControlDigitValid(invalidPnr);
+
+            // Assert
+            Assert.False(result);
         }
     }
 }
